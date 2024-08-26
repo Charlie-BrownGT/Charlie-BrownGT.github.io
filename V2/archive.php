af58da338f2c559e7feb014ddc9000e4711fe44a
Charlie-BrownGT <108026345+Charlie-BrownGT@users.noreply.github.com>
2024-08-26 23:27:59 +0100

    update

---
+++ V2/archive.php
@@ -0,0 +1,22 @@
+<?php
+    include_once 'header.php'
+?>
+
+<article>
+<h1>Archive</h1>
+    <br>
+    <p>
+    Please find content that's previously been published on this page. 
+    </p>
+    <br>
+    <p>
+    <ul>
+    <li><a display:inline href="./physics_projects">Physics Projects</a></li>
+    <li><a display:inline href="./computer_projects">Computer Projects</a></li>
+    </ul>
+    </p>
+</article>
+
+<?php
+    include_once 'footer.php'
+?> 
